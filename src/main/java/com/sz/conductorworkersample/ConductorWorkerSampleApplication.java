f83af9758a882d8f77a664c9be27bf5def8eda58
yanghaojia <2453883990@qq.com>
2023-11-13 17:31:08 +0800

    init

---
+++ src/main/java/com/sz/conductorworkersample/ConductorWorkerSampleApplication.java
@@ -0,0 +1,13 @@
+package com.sz.conductorworkersample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConductorWorkerSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConductorWorkerSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sz/conductorworkersample/ConductorWorkerSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sz.conductorworkersample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConductorWorkerSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
